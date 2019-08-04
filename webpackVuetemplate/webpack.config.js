495e2a5da97328dae97c9e99cfa4feb400e3484a
Oleksandr Kononenko <s_com@ukr.net>
2019-08-04 22:03:31 +0300

    dist, js import

--- webpackVuetemplate/webpack.config.js
+++ webpackVuetemplate/webpack.config.js
@@ -3,12 +3,13 @@ const path = require('path')
 module.exports = {
     entry :{
         app: './src/index.js'
+        // second: './src/index.js'
     },
     output: {
         // main.js -> [name].js -> app.js
         filename: '[name].js',
-        path: path.resolve(__dirname, './dist'),
-        publicPath:'./dist'
+        path: path.resolve(__dirname, '../dist'),
+        publicPath: '/dist'
     },
 
     devServer: {
