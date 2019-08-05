b1cb3fe6dcf2aaf0d21ca79cc0172ed415d62d5d
Oleksandr Kononenko <s_com@ukr.net>
2019-08-05 21:22:31 +0300

    build(dist,app.js); babel

--- webpackVuetemplate/src/js/common.js
+++ webpackVuetemplate/src/js/common.js
@@ -1,2 +1,2 @@
 let add = (a,b) => a+b
-console.log(add(2,3))
+console.log(add(2,6))
--- webpackVuetemplate/webpack.config.js
+++ webpackVuetemplate/webpack.config.js
@@ -8,9 +8,19 @@ module.exports = {
     output: {
         // main.js -> [name].js -> app.js
         filename: '[name].js',
-        path: path.resolve(__dirname, '../dist'),
+        path: path.resolve(__dirname, './dist'),
         publicPath: '/dist'
     },
+    
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: '/node_modules/'
+            }
+        ]
+    }
 
     devServer: {
         // warnings
