8afa0cf0fc4ee37bba7ef30382fdc54a54d636e4
Oleksandr Kononenko <s_com@ukr.net>
2019-08-07 21:35:30 +0300

    css-loader

--- webpackVuetemplate/webpack.config.js
+++ webpackVuetemplate/webpack.config.js
@@ -18,6 +18,11 @@ module.exports = {
                 test: /\.js$/,
                 loader: 'babel-loader',
                 exclude: '/node_modules/'
+            },
+            {
+                test: /\.css$/,
+                loader: 'css-loader',
+            
             }
         ]
     },
