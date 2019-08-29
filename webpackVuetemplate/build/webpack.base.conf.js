fe80c5985c941ba609c651fb0ace6f957f1f899b
Oleksandr Kononenko <s_com@ukr.net>
2019-08-29 20:32:16 +0300

    fonts.scss, base.conf

--- webpackVuetemplate/build/webpack.base.conf.js
+++ webpackVuetemplate/build/webpack.base.conf.js
@@ -70,6 +70,13 @@ module.exports = {
                 }
             },
             {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]'
+                }
+            },
+            {
                 test: /\.(png|jpg|gif|svg)$/,
                 loader: 'file-loader',
                 options: {
@@ -146,6 +153,7 @@ module.exports = {
           }),
           new CopyWebpackPlugin([
               {from: `${PATHS.src}/img`, to: `${PATHS.assets}img`},
+              {from: `${PATHS.src}/fonts`, to: `${PATHS.assets}fonts`},
               {from: `${PATHS.src}/static`, to: ``}
           ])
     ]
