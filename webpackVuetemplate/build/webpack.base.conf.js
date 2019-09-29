b88dfee20344bd72ff48491756f89a39f9b9f0e9
Oleksandr Kononenko <s_com@ukr.net>
2019-09-29 22:19:49 +0300

    for import css/scss libs

--- webpackVuetemplate/build/webpack.base.conf.js
+++ webpackVuetemplate/build/webpack.base.conf.js
@@ -4,7 +4,7 @@
 //  include path as module
 const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-
+ 
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
